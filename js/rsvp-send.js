c87333cb2806017bf31732e9de8f76aae94e74c6
cristafra <cristanelli.francesco@gmail.com>
2024-01-03 12:53:09 +0100

    03-01-2024 2
    new form v2

--- js/rsvp-send.js
+++ js/rsvp-send.js
@@ -1,28 +1,41 @@
 
 
-const sendForm = async () => {
+async function sendForm() {
 
     console.log("ciao qui");
-    var body = {
+    var data = {
                     "name":document.getElementById("name").value,
                     "phone":document.getElementById("phone").value,
                     "email":document.getElementById("email").value,
                     "guests":document.getElementById("guests").value,
                     "names_companions":document.getElementById("names_companions").value,
                     "food":document.getElementById("food").value,
-                    "msg":document.getElementById("msg").value,
+                    "message":document.getElementById("message").value,
                 }; // string or object
-    console.log(body);
-    
-    const response = await fetch('https://script.google.com/macros/s/AKfycbz8PCIrZFjUxN2SU0D_-2gcLMilWYOXDhD5vLDZuBw4rFMuE8zfqBqQGRlXzr2CO0uh/exec', {
-      method: 'POST',
-      body:body,
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-    const myJson = await response.json(); //extract JSON from the http response
+    console.log(data);
+    try {
+        const response = await fetch('https://script.google.com/macros/s/AKfycbz8PCIrZFjUxN2SU0D_-2gcLMilWYOXDhD5vLDZuBw4rFMuE8zfqBqQGRlXzr2CO0uh/exec'
+        , {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin':'*',
+            'Access-Control-Allow-Methods':'POST',
+        },
+        mode:"no-cors"
+        });
+        console.log(response)
+       // const result = await response.json()
+    }
+    catch(error)
+        {console.log(error)}
+
+
+    //const myJson = await response.json(); //extract JSON from the http response
     // do something with myJson
+    
+    console.log("finito");
   }
 
 var form = document.getElementById("myForm");
